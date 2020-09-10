a2517e189324c507664da078126317eafaaff683
JCZajeq <41554065+JCZajeq@users.noreply.github.com>
2020-09-10 22:48:44 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int  n = 1;
+bool prime = false;
+int main()
+{
+    while (true)
+    {
+        for (int i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                cout << "dzielnik: " << i << endl;
+                prime = false;
+                break;
+            }
+            else  prime = true;
+        }
+        if (prime == true) cout << "pierwsza: " << n << endl;
+        n += 2;
+    }
+}
